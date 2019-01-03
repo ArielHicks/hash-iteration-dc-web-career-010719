12dd70ead8f5c85b6068c6a47e082f23920d039d
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-03 00:26:02 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,9 +16,13 @@ def select_winner(passengers)
   winner
 end
 
-def happy_birthday
-  birthday_kids = {
-    "Timmy" => 9,
-    "Sarah" => 6,
-    "Amanda" => 27
-  }
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
